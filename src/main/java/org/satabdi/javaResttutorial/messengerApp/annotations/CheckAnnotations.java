aa32c400480667e277ea24998d14bd8a23b57f7f
satabdi.chakraborty <satabdi.d@gmail.com>
2018-09-21 19:23:37 -0400

    Header Param Test
    Header Param Test

--- src/main/java/org/satabdi/javaResttutorial/messengerApp/annotations/CheckAnnotations.java
+++ src/main/java/org/satabdi/javaResttutorial/messengerApp/annotations/CheckAnnotations.java
@@ -2,6 +2,7 @@ package org.satabdi.javaResttutorial.messengerApp.annotations;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -16,6 +17,15 @@ public class CheckAnnotations {
 	@GET
 	public String testMatrixParams(@MatrixParam("param") String param) {
 		//Test URL : http://localhost:8080/messengerApp/webapi/annotations;param=hello
+		return param;
+	}
+	
+	@GET
+	@Path("headerParam")
+	public String testHeaderParams(@HeaderParam("headerParam") String param) {
+		//Test URL : http://localhost:8080/messengerApp/webapi/annotations/headerParam
+		//headerParam=Satabdi in postman header
+		System.out.println("Received Header Param : "+param);
 		return param;
 	}
 
